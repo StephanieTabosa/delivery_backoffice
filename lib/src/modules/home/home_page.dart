c5c461f2735de64fe824b400fcb987ea1e3dea14
stephanie <stephanie.barreto@hotmail.com>
2023-05-10 22:15:24 -0300

    packages, helpers, env

---
+++ lib/src/modules/home/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
+import '../../core/ui/helpers/size_extensions.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Text(
+              context.screenWidth.toString(),
+            ),
+            Container(
+              color: Colors.amber,
+              height: context.percentHeight(.9),
+              width: context.percentWidth(.5),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(const Duration(seconds: 2));
+          hideLoader();
+          showSuccess('Carregou corretamente');
+        },
+      ),
+    );
+  }
+}
