46290bab692b07674470df3ff7aa8dc43e066a5e
stephanie <stephanie.barreto@hotmail.com>
2023-05-15 23:02:36 -0300

    creates login page and module

--- lib/src/modules/login/login_page.dart
+++ lib/src/modules/login/login_page.dart
@@ -1,15 +1,106 @@
 import 'package:flutter/material.dart';
 
+import '../../core/ui/helpers/size_extensions.dart';
+import '../../core/ui/styles/app_colors.dart';
+import '../../core/ui/styles/app_text_styles.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final screenShortestSide = context.screenShortestSide;
+    final screenWidth = context.screenWidth;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+      backgroundColor: context.colors.black,
+      body: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: screenShortestSide * .5,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/lanche.png',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: context.percentHeight(.10)),
+            width: screenShortestSide * .5,
+            child: Image.asset('assets/images/logo.png'),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: context.percentWidth(screenWidth < 1300 ? .7 : .3),
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(30),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      FractionallySizedBox(
+                        widthFactor: .3,
+                        child: Image.asset('assets/images/logo.png'),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      FittedBox(
+                        fit: BoxFit.scaleDown,
+                        child: Text(
+                          'Login',
+                          style: context.appTextStyles.textTitle,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'E-mail',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Senha',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      SizedBox(
+                        height: 50,
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Entrar'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
